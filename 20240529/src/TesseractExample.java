8659498fd67f1efc9d1d7f88e5b09a56a0662375
George920825 <84559855+George920825@users.noreply.github.com>
2024-05-29 22:30:09 +0800

    Add files via upload

---
+++ 20240529/src/TesseractExample.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import net.sourceforge.tess4j.*;
+
+public class TesseractExample {
+
+    public static void main(String[] args) {
+
+        File imageFile = new File("sample2.png");
+        ITesseract instance = new Tesseract();  // JNA Interface Mapping
+        // ITesseract instance = new Tesseract1(); // JNA Direct Mapping
+
+        // For Chinese
+//        instance.setLanguage("chi_tra");
+
+        // Set datapath
+        instance.setDatapath("Tess4J/tessdata"); // path to tessdata directory
+
+        try {
+            String result = instance.doOCR(imageFile);
+            System.out.println(result);
+        } catch (TesseractException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
