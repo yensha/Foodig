b8de7c91817d75d6e8c53664d6f91afaa7eb2dd3
yensha <113085268+yensha@users.noreply.github.com>
2024-06-03 23:52:17 +0800

    m

--- src/main/java/org/example/foodig_v2/MouseController.java
+++ src/main/java/org/example/foodig_v2/MouseController.java
@@ -1,13 +1,18 @@
 package org.example.foodig_v2;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.io.IOException;
 
 public class MouseController implements Initializable {
 
+    @FXML
+    ImageView Image_Shop;
 
 
     @Override
