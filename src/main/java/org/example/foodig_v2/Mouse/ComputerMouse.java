47ec70b77b5d65ffa80d60f25c0c09c1263dcce4
yensha <113085268+yensha@users.noreply.github.com>
2024-06-05 02:46:04 +0800

    update the mouse

--- src/main/java/org/example/foodig_v2/Mouse/ComputerMouse.java
+++ src/main/java/org/example/foodig_v2/Mouse/ComputerMouse.java
@@ -3,7 +3,7 @@ package org.example.foodig_v2.Mouse;
 public class ComputerMouse extends Mouse {
     private String imagePath = "org/example/foodig_v2/image/mouse3.png";
     public ComputerMouse() {
-        super("ComputerMouse", 16, 1.0, false);
+        super("ComputerMouse", 16, 1.0, false, "image/mouse3.png");
     }
     @Override
     public String getImagePath() {
