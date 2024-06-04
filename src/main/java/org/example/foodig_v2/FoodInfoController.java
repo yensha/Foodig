dbe5dfb9533edbdcf038772d2adf359f94e7cfb1
George920825 <ycm089457@gmail.com>
2024-06-04 22:02:43 +0800

    Modify image path,  add foodinfo listener

--- src/main/java/org/example/foodig_v2/FoodInfoController.java
+++ src/main/java/org/example/foodig_v2/FoodInfoController.java
@@ -1,5 +1,7 @@
 package org.example.foodig_v2;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -19,14 +21,49 @@ import static org.example.foodig_v2.FoodStorage.*;
 
 public class FoodInfoController implements Initializable {
 
-    @FXML
-    private ChoiceBox<String> ChoiceBox_FoodType;
-    private final String[] foodType = {"全穀雜糧", "豆魚蛋肉", "蔬菜", "水果", "乳品", "調味料"};
     @Override
     public void initialize(URL arg0, ResourceBundle arg1){
 
         ChoiceBox_FoodType.getItems().addAll(foodType);
         scene_controller_food = new SceneController();
+
+        DatePicker_ExpiredDate.valueProperty().addListener(new ChangeListener<LocalDate>() {
+            @Override
+            public void changed(ObservableValue<? extends LocalDate> observableValue, LocalDate oldValue, LocalDate newValue) {
+                if (newValue != null) {
+                    LocalDate today = LocalDate.now();
+                    LocalDate threeDaysAfter = today.plusDays(3);
+                    if (today.isAfter(newValue)) {
+                        Label_FoodStatus.setText("過期囉");
+                    } else if (threeDaysAfter.isAfter(newValue)){
+                        Label_FoodStatus.setText("即將到期");
+                    } else {
+                        Label_FoodStatus.setText("尚未到期");
+                    }
+                }
+                else {
+                    Label_FoodStatus.setText("尚未到期");
+                }
+            }
+        });
+
+        ChoiceBox_FoodType.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                switch (newValue) {
+                    case "全穀雜糧" -> ImageView_FoodImage.setImage(new Image(String.valueOf(getClass().getResource("image/rice.png"))));
+                    case "豆魚蛋肉" -> ImageView_FoodImage.setImage(new Image(String.valueOf(getClass().getResource("image/protein.png"))));
+                    case "蔬菜" -> ImageView_FoodImage.setImage(new Image(String.valueOf(getClass().getResource("image/vegetable.png"))));
+                    case "水果" -> ImageView_FoodImage.setImage(new Image(String.valueOf(getClass().getResource("image/fruits.png"))));
+                    case "乳品" -> ImageView_FoodImage.setImage(new Image(String.valueOf(getClass().getResource("image/dairy-products.png"))));
+                    case "調味料" -> ImageView_FoodImage.setImage(new Image(String.valueOf(getClass().getResource("image/spices.png"))));
+                    default -> ImageView_FoodImage.setImage(new Image(String.valueOf(getClass().getResource("image/food-service.png"))));
+                }
+            }
+            else {
+                ImageView_FoodImage.setImage(new Image(String.valueOf(getClass().getResource("image/food-service.png"))));
+            }
+        });
+
     }
     // not done yet!!! https://youtu.be/hwCbXOM4_Qc?si=C609OQE9uiolYzhe
 
@@ -59,6 +96,9 @@ public class FoodInfoController implements Initializable {
     private DatePicker DatePicker_ExpiredDate;
     @FXML
     private DatePicker DatePicker_ManufactureDate;
+    @FXML
+    private ChoiceBox<String> ChoiceBox_FoodType;
+    private final String[] foodType = {"全穀雜糧", "豆魚蛋肉", "蔬菜", "水果", "乳品", "調味料"};
     @FXML
     private TextArea TextArea_FoodRemark;
     @FXML
