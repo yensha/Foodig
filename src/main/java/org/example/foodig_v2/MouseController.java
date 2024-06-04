93c864d39dd4b38e23ecbd9d4d3cfb9358bcf965
George920825 <ycm089457@gmail.com>
2024-06-05 04:48:58 +0800

    satietyArray image

--- src/main/java/org/example/foodig_v2/MouseController.java
+++ src/main/java/org/example/foodig_v2/MouseController.java
@@ -15,6 +15,7 @@ import org.example.foodig_v2.Mouse.Mouse;
 import org.example.foodig_v2.MouseFarm;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import java.io.IOException;
@@ -23,8 +24,7 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 import static java.util.concurrent.Executors.newSingleThreadScheduledExecutor;
-import static org.example.foodig_v2.MouseFarm.PetMouse;
-import static org.example.foodig_v2.MouseFarm.feedFood;
+import static org.example.foodig_v2.MouseFarm.*;
 
 public class MouseController implements Initializable {
 
@@ -49,9 +49,24 @@ public class MouseController implements Initializable {
     @FXML
     Button Collectbtn;
 
+    @FXML
+    private ImageView satiety1;
+
+    @FXML
+    private ImageView satiety2;
+
+    @FXML
+    private ImageView satiety3;
+
+    @FXML
+    private ImageView satiety4;
+
+    @FXML
+    private ImageView satiety5;
+
     private SceneController scenecontroller;
 
-
+    private ArrayList<ImageView> satietyArray = new ArrayList<>();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -59,11 +74,27 @@ public class MouseController implements Initializable {
         scenecontroller = new SceneController();
         Coin_Label.setText(String.valueOf(MouseFarm.Coin));
         Feed_Label.setText(String.valueOf(MouseFarm.Feed));
+        SatietyTimer();
+
+        satietyArray.add(satiety1);
+        satietyArray.add(satiety2);
+        satietyArray.add(satiety3);
+        satietyArray.add(satiety4);
+        satietyArray.add(satiety5);
 
         AnimationTimer timer = new AnimationTimer() {
             @Override
             public void handle(long l) {
+                Feed_Label.setText(String.valueOf(MouseFarm.Feed));
+                Feedbtn.setDisable(Feed <= 0);
                 Collectbtn.setDisable(PetMouse.ispoison());
+                int setcounter = (int) ((PetMouse.getSatiety()-1.0)*10);
+                for (int i = 0; i < 5; i++) {
+                    satietyArray.get(i).setOpacity(0.2);
+                }
+                for (int i = 0; i < setcounter; i++) {
+                    satietyArray.get(i).setOpacity(1.0);
+                }
             }
         };
         timer.start();
